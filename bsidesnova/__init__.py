e4345d7583fde1f94fddf84c53b7bbd61c875fa8
pavanreddyml <pavanreddyml@users.noreply.github.com>
2025-10-07 04:42:08 -0400

    init

--- bsidesnova/__init__.py
+++ bsidesnova/__init__.py
@@ -4,5 +4,6 @@ from .ollama_client import OllamaClient
 from .exfil_server import ExfilServer
 from .context_fetchers import ContextFetcher
 from .selectors import Selector
+from .widgets import AttackSelectorWidget
 
-__all__ = ["DatabaseUI", "SQLUtils", "OllamaClient", "ExfilServer", "ContextFetcher", "Selector"]
+__all__ = ["DatabaseUI", "SQLUtils", "OllamaClient", "ExfilServer", "ContextFetcher", "Selector", "AttackSelectorWidget"]
